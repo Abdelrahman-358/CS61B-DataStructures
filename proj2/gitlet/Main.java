6d0f732df7839b22b1c104490891273745fb3984
Abdelrahman Mostafa <abdomosfa2004@gmail.com>
2025-02-06 08:47:06 +0200

    updating Main class

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -9,33 +9,42 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
-        // TODO: handle errors that related to args size
+        if (args.length == 0) {
+            Repository.errorMessage("Please enter a command.");
+            return;
+        }
+
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
-                //initializing gitlet repo
                 Repository.init();
                 break;
             case "add":
-                //add file
-                if(args.length == 2)
-                Repository.add(args[1]);
-                else Repository.errorMessage("not enough arguments");
+                if (args.length == 2) {
+                    Repository.add(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
             case "commit":
-                if(args.length == 2 && args[1]!="")
-                Repository.commit(args[1]);
-                else Repository.errorMessage("Please enter a commit message.");
+                if (args.length == 2 && !args[1].equals("")) {
+                    Repository.commit(args[1]);
+                } else {
+                    Repository.errorMessage("Please enter a commit message.");
+                }
                 break;
             case "rm":
-                if(args.length == 2){
-                    //remove file
+                if (args.length == 2) {
                     Repository.rm(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
                 }
-                else {
-                    //remove branch
-                    Repository.rmBranch(args[3]);
+                break;
+            case "rm-branch":
+                if (args.length == 2) {
+                    Repository.rmBranch(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
                 }
                 break;
             case "log":
@@ -45,38 +54,57 @@ public class Main {
                 Repository.global_log();
                 break;
             case "find":
-                Repository.find(args[1]);
+                if (args.length == 2) {
+                    Repository.find(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
             case "status":
                 Repository.status();
                 break;
             case "checkout":
-                if(args[1].equals("--")){
+                if (args.length == 3 && args[1].equals("--")) {
                     Repository.checkout(args[2]);
-                }else if(args.length>2){
-                    Repository.checkout(args[1],args[3]);
-                }else if(args.length==2) {
+                } else if (args.length == 4 && args[2].equals("--")) {
+                    Repository.checkout(args[1], args[3]);
+                } else if (args.length == 2) {
                     Repository.checkoutBranch(args[1]);
-                }else{
+                } else {
                     Repository.errorMessage("Incorrect operands.");
                 }
                 break;
             case "branch":
-                Repository.branch(args[1]);
+                if (args.length == 2) {
+                    Repository.branch(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
             case "reset":
-                if(args.length == 2)
-                Repository.reset(args[1]);
-                else Repository.errorMessage("not enough arguments");
+                if (args.length == 2) {
+                    Repository.reset(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
             case "merge":
-                Repository.merge(args[1]);
+                if (args.length == 2) {
+                    Repository.merge(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
             case "get":
-                Repository.get(args[1]);
+                if (args.length == 2) {
+                    Repository.get(args[1]);
+                } else {
+                    Repository.errorMessage("Not enough arguments.");
+                }
                 break;
-
-
+            default:
+                Repository.errorMessage("No command with that name exists.");
+                break;
         }
     }
 }
